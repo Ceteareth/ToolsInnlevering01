199a857eb8b865c737465ef86adc574068ab3d49
Raymond Gulbrandsen <raymond.gulbr@gmail.com>
2013-10-11 18:06:19 +0200

    added tile class

---
+++ Innlevering01/Tile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Innlevering01
+{
+    class Tile
+    {
+        public string tileName
+        {
+            get { return tileName; }
+            internal set { tileName = value; } 
+        }
+
+        public imagenode imagenode
+        {
+            get { return imagenode; }
+            internal set { imagenode = value; }
+        }
+
+        private char[,] collisionMap = new char[3,3];
+
+        private void setCollisionMap(char[,] value)
+        {
+            collisionMap = value;
+        }
+
+        public char[,] getCollisionMap()
+        {
+            return collisionMap;
+        }
+
+        public void Tile(ImageNode _imageNode,string _tileName, char[,] _collisionMap)
+        {
+            this.imageNode = _imageNode;
+            this.tileName = _tileName;
+            setCollisionMap(_collisionMap);
+        }
+        
+    }
+}
